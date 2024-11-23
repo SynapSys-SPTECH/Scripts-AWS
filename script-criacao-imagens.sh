fa0a4fbe48dcb676b2df4eced3f9f52d4b9bbe6c
GuiGavassa <guilhermegavassa.tecn@gmail.com>
2024-11-23 13:44:23 -0300

    scripts atuais

---
+++ script-criacao-imagens.sh
@@ -0,0 +1,154 @@
+  GNU nano 7.2                                                                                                                                                                                                                                                                                                                                                                                                                                                                   script-criacao-imagens.sh
+#!/bin/bash
+
+# Variáveis de containers e rede
+MYSQL_CONTAINER="mysql-synapsys"
+NODE_CONTAINER="node-synapsys"
+JAVA_CONTAINER="java-synapsys"
+NETWORK_NAME="redeSynapsys"
+
+# Variáveis de configuração do MySQL
+MYSQL_ROOT_PASSWORD="urubu100"
+MYSQL_DATABASE="Synapsys"
+
+# Variáveis de ambiente para o container Java
+DB_URL="jdbc:mysql://172.18.0.2:3306/Synapsys"
+DB_USERNAME="root"
+DB_PASSWORD="urubu100"
+NAME_BUCKET="bucket-synapsys"
+
+# Verificação e instalação do Docker
+echo "Verificando se o Docker está instalado..."
+if ! [ -x "$(command -v docker)" ]; then
+    echo "Docker não está instalado. Instalando Docker..."
+    sudo apt update
+    sudo apt install -y docker.io
+else
+    echo "Docker já está instalado. Realizando upgrade..."
+    sudo apt update
+    sudo apt upgrade -y docker.io
+fi
+
+# Função para verificar se o container existe
+function container_exists() {
+    docker ps -a --format '{{.Names}}' | grep -Eq "^${1}$"
+}
+
+# Criar rede se não existir
+if ! docker network ls --format '{{.Name}}' | grep -Eq "^${NETWORK_NAME}$"; then
+    echo "Criando rede $NETWORK_NAME..."
+    docker network create $NETWORK_NAME
+else
+    echo "Rede $NETWORK_NAME já existe."
+fi
+
+# Verificação e cópia do arquivo SQL
+mkdir -p init-db
+if [ -f "projeto/bancoProjeto/script-tabelas.sql" ]; then
+    echo "Dando Pull e Copiando script-tabelas.sql de projeto/bancoProjeto/script-tabelas.sql para init-db..."
+    cd projeto/bancoProjeto
+    git pull
+    cp "projeto/bancoProjeto/script-tabelas.sql" init-db/
+else
+    echo "O arquivo script.sql não foi encontrado. Clonando Repositório de MYSQL-Synapsys."
+    mkdir -p projeto/bancoProjeto
+    rm -rf projeto/bancoProjeto
+    git clone https://github.com/SynapSys-SPTECH/Banco-de-Dados.git projeto/bancoProjeto
+    git pull
+    cp "projeto/bancoProjeto/script-tabelas.sql" init-db/
+fi
+
+# Criação e verificação do container MySQL
+if container_exists $MYSQL_CONTAINER; then
+    if docker ps --format '{{.Names}}' | grep -Eq "^${MYSQL_CONTAINER}$"; then
+        echo "Container $MYSQL_CONTAINER já está rodando."
+    else
+        echo "Iniciando container $MYSQL_CONTAINER..."
+        docker start $MYSQL_CONTAINER
+    fi
+else
+    echo "Criando e rodando container $MYSQL_CONTAINER..."
+    docker run -d \
+        --name $MYSQL_CONTAINER \
+        --network $NETWORK_NAME \
+        -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+        -e MYSQL_DATABASE=$MYSQL_DATABASE \
+        -v $(pwd)/init-db:/docker-entrypoint-initdb.d \
+        -p 3306:3306 \
+        mysql:8.0
+    echo "Aguardando MySQL iniciar..."
+    sleep 5
+fi
+
+
+# Criação e verificação do container Node
+if container_exists $NODE_CONTAINER; then
+    if docker ps --format '{{.Names}}' | grep -Eq "^${NODE_CONTAINER}$"; then
+        echo "Container $NODE_CONTAINER já está rodando. Atualizando o repositório..."
+        cd projeto/Projeto-Synapsys && git pull
+    else
+        echo "Iniciando container $NODE_CONTAINER e atualizando o repositório..."
+        cd projeto/Projeto-Synapsys && git pull
+        docker start $NODE_CONTAINER
+    fi
+else
+    echo "Clonando repositório e configurando container Node..."
+    mkdir -p projeto
+    rm -rf projeto/Projeto-Synapsys
+    git clone -b dev-sprint-3 https://github.com/SynapSys-SPTECH/Site-Institucional.git projeto/Projeto-Synapsys
+
+    cat <<EOF > projeto/Projeto-Synapsys/Dockerfile
+FROM node:20
+
+# Define o diretório de trabalho na raiz do projeto
+WORKDIR /app
+
+# Instala git e clona o repositório diretamente no Dockerfile
+RUN apt-get update && apt-get install -y git && \
+    git clone -b dev-sprint-3 https://github.com/SynapSys-SPTECH/Site-Institucional.git .
+
+RUN git pull
+
+# Instala as dependências
+RUN npm install
+
+# Exponha a porta necessária
+EXPOSE 3333
+
+# Comando de inicialização
+CMD ["npm", "start"]
+EOF
+
+    # Constrói e executa o container Node
+    cd projeto/Projeto-Synapsys
+    docker build -t node-synapsys-image .
+    docker run -d --name $NODE_CONTAINER --network $NETWORK_NAME -p 3333:3333 node-synapsys-image
+fi
+
+# Criação e verificação do container Java
+if container_exists $JAVA_CONTAINER; then
+    echo "Container $JAVA_CONTAINER já existe. Iniciando..."
+    docker start $JAVA_CONTAINER
+else
+    echo "Configurando container Java..."
+    mkdir -p javaProjeto/Synapsys-Java
+
+    cat <<EOF > javaProjeto/Synapsys-Java/Dockerfile
+FROM openjdk:21-jdk-bullseye
+SHELL ["/bin/bash", "-c"]
+RUN if [ ! -d "/app" ]; then apt-get update && apt-get install -y git && git clone -b feature/BD-AWS https://github.com/SynapSys-SPTECH/Java.git /app; fi
+WORKDIR /app/target
+ENV URL_SQL=$DB_URL
+ENV USER=$DB_USERNAME
+ENV SENHA=$DB_PASSWORD
+ENV NAME_BUCKET=$NAME_BUCKET
+EXPOSE 8080
+CMD ["java", "-jar", "java-project-synapsys-2.0-SNAPSHOT-jar-with-dependencies.jar"]
+EOF
+
+    cd javaProjeto/Synapsys-Java
+    docker build -t script-java .
+    docker run -d --name $JAVA_CONTAINER --network $NETWORK_NAME -e URL_SQL=$DB_URL -e USER=$DB_USERNAME -e SENHA=$DB_PASSWORD script-java
+fi
+
+echo "Containers MySQL, Node e Java foram verificados e iniciados (ou criados, se necessário)."
---
+++ start-containers.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Função para verificar se um comando existe
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Verificando se o Docker está instalado
+if ! command_exists docker; then
+    echo "Docker não está instalado. Instalando..."
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+else
+    echo "Docker já está instalado."
+fi
+
+# Verificando se o Docker Compose está instalado
+if ! command_exists docker-compose; then
+    echo "Docker Compose não está instalado. Instalando..."
+    sudo apt-get install -y docker-compose
+else
+    echo "Docker Compose já está instalado."
+fi
+
+# Função para verificar se um container está em execução
+check_container_running() {
+  docker ps --filter "name=$1" --format '{{.Names}}' | grep -w "$1" > /dev/null
+}
+
+# Função para verificar se um container existe
+check_container_exists() {
+  docker ps -a --filter "name=$1" --format '{{.Names}}' | grep -w "$1" > /dev/null
+}
+
+# Nome dos serviços definidos no docker-compose.yml
+service_names=("mysql-synapsys" "node-synapsys" "java-synapsys")
+
+# Loop pelos serviços para verificar se precisam ser construídos ou iniciados
+for service in "${service_names[@]}"; do
+  if check_container_exists "$service"; then
+    echo "O container $service já existe."
+
+    if check_container_running "$service"; then
+      echo "O container $service já está em execução."
+    else
+      echo "Removendo o container $service e iniciando novamente..."
+      sudo docker start "$service"
+    fi
+
+  else
+    echo "Construindo e iniciando o container $service..."
+    sudo docker-compose build "$service"
+    sudo docker-compose up -d "$service"
+  fi
+done
+
+
+# Verificando o status dos containers
+echo "Verificando o status dos containers..." 
+docker-compose ps -a
+
+
+# Verificando logs dos containers
+echo "Exibindo logs do container mysql-synapsys..."
+docker logs mysql-synapsys
+
+echo "Exibindo logs do container node-synapsys..."
+docker logs node-synapsys
+
+echo "Exibindo logs do container java-synapsys..."
+docker logs java-synapsys
+
+# Fim do script
+echo "Tudo configurado e containers estão em execução!"
